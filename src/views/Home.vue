b3c4ae408afe14faebb30de48c20d72a12903e86
laura <l.keeley000@gmail.com>
2021-10-15 11:42:37 -0500

    init commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
+<style></style>
+<script src="https://unpkg.com/@popperjs/core@2"></script>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { createPopper } from "@popperjs/core";
+// Popper.Defaults.modifiers.computeStyle.gpuAcceleration = false;
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  mounted() {
+    this.$nextTick(function () {
+      var popper = new Popper(
+        this.$refs["showInputButton"],
+        this.$refs["dropdown"],
+        {
+          placement: "bottom-start",
+        }
+      );
+      console.log(popper);
+    });
   },
+  data: function () {
+    return {
+      message: "Button Tester Page",
+    };
+  },
+  created: function () {},
+  methods: {},
 };
 </script>
